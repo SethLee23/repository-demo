7a1fe28aac3d1d41fda1d9bf1c9da46bd829965f
seth <1532084282@qq.com>
2020-07-15 09:42:26 +0800

    feat: 修改配置
    修改工作区eslint/prettier配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,27 @@
+/*
+ * @Author: your name
+ * @Date: 2020-07-15 09:28:57
+ * @LastEditTime: 2020-07-15 09:40:42
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \web\.eslintrc.js
+ */
 module.exports = {
   root: true,
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
-  parserOptions: {
-    parser: "babel-eslint"
-  },
+  extends: ['plugin:vue/essential', 'eslint:recommended'],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    //强制使用单引号
+    quotes: ['error', 'single'],
+    //强制不使用分号结尾
+    semi: ['error', 'always']
   },
-  overrides: [
-    {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
-      ],
-      env: {
-        jest: true
-      }
-    }
-  ]
-};
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+}
+
--- src/api/api.js
+++ src/api/api.js
@@ -1,22 +1,14 @@
-/*
- * @Author: seth
- * @Date: 2020-04-28 16:28:58
- * @LastEditTime: 2020-06-20 19:12:32
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: \dyd-h5web\src\api\api.js
- */
-import request from "./request";
+import request from './request';
 // 接口封装
 const api = {
   articleList: data => {
-    return request("informationList", data, "get");
+    return request('informationList', data, 'get');
   }, // 热点列表
   getHotDetail: data => {
-    return request("informationRead", data, "get");
+    return request('informationRead', data, 'get');
   }, // 热点详情
   articleCat: data => {
-    return request("informationType", data, "get");
+    return request('informationType', data, 'get');
   } // 热点-文章类型列表
 };
 
