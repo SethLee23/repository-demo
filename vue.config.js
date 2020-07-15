85d0a70564c22641e6bde04f6a606d94d5379123
seth <1532084282@qq.com>
2020-07-15 16:45:38 +0800

    feat: 修改配置
    修改按需引入,分析配置

--- vue.config.js
+++ vue.config.js
@@ -1,35 +1,38 @@
 // vue.config.js
 module.exports = {
-  publicPath: "./", //vue-cli3.3+新版本使用
+  publicPath: './', //vue-cli3.3+新版本使用
   //输出文件目录
-  outputDir: process.env.outputDir || "dist",
+  outputDir: process.env.outputDir || 'dist',
   // eslint-loader 是否在保存的时候检查
   lintOnSave: true,
   //放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录。
-  assetsDir: "static",
-  // devServer: {
-  //   open: true,
-  //   host: 'localhost',
-  //   port: 8080,
-  //   https: false,
-  //   //以上的ip和端口是我们本机的;下面为需要跨域的
-  //   proxy: {  //配置跨域
-  //     '/api': {
-  //       target: '',  //这里后台的地址模拟的;应该填写你们真实的后台接口
-  //       ws: true,
-  //       changOrigin: true,  //允许跨域
-  //       pathRewrite: {
-  //         '^/api': ''  //请求的时候使用这个api就可以
-  //         // '^api': ''
-  //       }
-  //     }
-  //   }
-  // },
+  assetsDir: 'static',
+  devServer: {
+    open: true,
+    host: '0.0.0.0',
+    port: 8080,
+    https: false,
+    //以上的ip和端口是我们本机的;下面为需要跨域的
+    // proxy: {  //配置跨域
+    //   '/api': {
+    //     target: '',  //这里后台的地址模拟的;应该填写你们真实的后台接口
+    //     ws: true,
+    //     changOrigin: true,  //允许跨域
+    //     pathRewrite: {
+    //       '^/api': ''  //请求的时候使用这个api就可以
+    //       // '^api': ''
+    //     }
+    //   }
+    // }
+  },
   chainWebpack: config => {
     // 这里是对环境的配置，不同环境对应不同的BASE_URL，以便axios的请求地址不同
-    config.plugin("define").tap(args => {
-      args[0]["process.env"].BASE_URL = JSON.stringify(process.env.BASE_URL);
+    config.plugin('define').tap(args => {
+      args[0]['process.env'].BASE_URL = JSON.stringify(process.env.BASE_URL);
       return args;
     });
+    config
+      .plugin('webpack-bundle-analyzer')
+      .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin);
   }
 };
